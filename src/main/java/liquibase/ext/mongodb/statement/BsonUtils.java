165e81a30d3275e944f0ba77eb3bfd67f9bf4e5a
Lawrence Mouarkach <lawrence.mouarkach@hotmail.com>
2021-01-06 12:35:59 +0000

    Update BsonUtils.java

--- src/main/java/liquibase/ext/mongodb/statement/BsonUtils.java
+++ src/main/java/liquibase/ext/mongodb/statement/BsonUtils.java
@@ -32,6 +32,7 @@ import org.bson.codecs.configuration.CodecRegistry;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.nonNull;
 import static java.util.Optional.ofNullable;
